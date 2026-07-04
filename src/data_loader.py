f48f8fb8c9fea4935aa4b202af35c8bf091f25cd
Ronald Sierra <rsier033@fiu.edu>
2026-07-04 14:05:35 -0400

    update data_loader.py
    first implementation of being able to fetch binary data from MINST training files.

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,3 +1,13 @@
-import gzip
 import struct
+
+def read_labels(filepath):
+    with open(filepath, 'rb') as f:
+        magic, num_labels = struct.unpack('>II', f.read(8))
+        labels = f.read()
+    return labels
+
+
+if __name__ == '__main__':
+    result = read_labels('../data/train-labels.idx1-ubyte')
+    print(result[:10])
 
