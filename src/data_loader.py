5e961b7f72e4f39166dd6a1d8823b49d09595d24
Ronald Sierra <rsier033@fiu.edu>
2026-07-05 15:37:32 -0400

    create read_images function

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,4 +1,5 @@
 import struct
+import numpy as np
 
 def read_labels(filepath):
     with open(filepath, 'rb') as f:
@@ -7,7 +8,17 @@ def read_labels(filepath):
     return labels
 
 
+
+
+def read_images(filepath):
+    with open(filepath, 'rb') as f:
+        magic, count, rows, columns = struct.unpack('>IIII', f.read(16))
+        images = np.frombuffer(f.read(), dtype=np.uint8)
+        images = images.reshape(count, rows, columns)
+    return images
+
 if __name__ == '__main__':
     result = read_labels('../data/train-labels.idx1-ubyte')
     print(result[:10])
-
+    result1 = read_images('../data/train-images.idx3-ubyte')
+    print(result1.shape)
